e7ee4a4a45b419315105af7aeff0db69102634aa
hezhang <zhanghe1979@gmail.com>
2024-06-18 14:33:44 -0400

    feat: define overloading math operators

--- include/symbolic.h
+++ include/symbolic.h
@@ -8,8 +8,106 @@
 bool is_zero(SymEngine::Expression x);
 void simplified_expr(SymEngine::Expression& x);
 
+//Overloading operator "/"
 inline SymEngine::Expression operator/(int i, const SymEngine::Expression expr) {
     return SymEngine::Expression(i)/expr;
 }
+
+inline SymEngine::Expression operator/(const SymEngine::Expression expr, int i) {
+    return expr/SymEngine::Expression(i);
+}
+
+inline SymEngine::Expression operator/(unsigned int i, const SymEngine::Expression expr) {
+    return SymEngine::Expression(i)/expr;
+}
+
+inline SymEngine::Expression operator/(const SymEngine::Expression expr, unsigned int i) {
+    return expr/SymEngine::Expression(i);
+}
+
+inline SymEngine::Expression operator/(double d, const SymEngine::Expression expr) {
+    return SymEngine::Expression(d)/expr;
+}
+
+inline SymEngine::Expression operator/(const SymEngine::Expression expr, double d) {
+    return expr/SymEngine::Expression(d);
+}
+
+//Overloading operator "*"
+inline SymEngine::Expression operator*(int i, const SymEngine::Expression expr) {
+    return SymEngine::Expression(i)*expr;
+}
+
+inline SymEngine::Expression operator*(const SymEngine::Expression expr, int i) {
+    return expr*SymEngine::Expression(i);
+}
+
+inline SymEngine::Expression operator*(unsigned int i, const SymEngine::Expression expr) {
+    return SymEngine::Expression(i)*expr;
+}
+
+inline SymEngine::Expression operator*(const SymEngine::Expression expr, unsigned int i) {
+    return expr*SymEngine::Expression(i);
+}
+
+inline SymEngine::Expression operator*(double d, const SymEngine::Expression expr) {
+    return SymEngine::Expression(d)*expr;
+}
+
+inline SymEngine::Expression operator*(const SymEngine::Expression expr, double d) {
+    return expr*SymEngine::Expression(d);
+}
+
+
+//Overloading operator "+"
+inline SymEngine::Expression operator+(int i, const SymEngine::Expression expr) {
+    return SymEngine::Expression(i)+expr;
+}
+
+inline SymEngine::Expression operator+(const SymEngine::Expression expr, int i) {
+    return expr+SymEngine::Expression(i);
+}
+
+inline SymEngine::Expression operator+(unsigned int i, const SymEngine::Expression expr) {
+    return SymEngine::Expression(i)+expr;
+}
+
+inline SymEngine::Expression operator+(const SymEngine::Expression expr, unsigned int i) {
+    return expr+SymEngine::Expression(i);
+}
+
+inline SymEngine::Expression operator+(double d, const SymEngine::Expression expr) {
+    return SymEngine::Expression(d)+expr;
+}
+
+inline SymEngine::Expression operator+(const SymEngine::Expression expr, double d) {
+    return expr+SymEngine::Expression(d);
+}
+
+//Overloading operator "-"
+inline SymEngine::Expression operator-(int i, const SymEngine::Expression expr) {
+    return SymEngine::Expression(i)-expr;
+}
+
+inline SymEngine::Expression operator-(const SymEngine::Expression expr, int i) {
+    return expr-SymEngine::Expression(i);
+}
+
+inline SymEngine::Expression operator-(unsigned int i, const SymEngine::Expression expr) {
+    return SymEngine::Expression(i)-expr;
+}
+
+inline SymEngine::Expression operator-(const SymEngine::Expression expr, unsigned int i) {
+    return expr-SymEngine::Expression(i);
+}
+
+inline SymEngine::Expression operator-(double d, const SymEngine::Expression expr) {
+    return SymEngine::Expression(d)-expr;
+}
+
+inline SymEngine::Expression operator-(const SymEngine::Expression expr, double d) {
+    return expr-SymEngine::Expression(d);
+}
+
 
 #endif // SYMBOLIC
