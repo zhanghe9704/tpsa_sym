30902ef93c059cabc51b203da5b51787ad7a53a4
He Zhang <zhanghe1979@gmail.com>
2024-10-09 22:24:52 -0400

    feat: adding tests

--- tests/tests.cc
+++ tests/tests.cc
@@ -3,82 +3,148 @@
 #include "sda.h"
 
 #include <algorithm>
-#include <complex>
-#include <fstream>
-#include <sstream>
-#include <string>
+#include <array>
+#include <cmath>
+// #include <complex>
+// #include <fstream>
+// #include <sstream>
+// #include <string>
+#include <vector>
+#include <iomanip>
 
 typedef SymbDA::DAVector SDA;
-using std::complex;
-using std::string;
-using namespace std::complex_literals;
+// using std::complex;
+// using std::string;
+typedef SymbDA::DAVector SDA;
+using SymEngine::Expression;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::array;
+// using namespace std::complex_literals;
+
+
+bool compare_vectors(vector<double>& a, vector<double>& b, double eps) {
+    int l1 = a.size();
+    int l2 = b.size();
+    if (l1 != l2) return false;
+    for(int i=0; i<l1; ++i) {
+        double d = fabs(a.at(i)-b.at(i));
+        if (d>eps) d /= fabs(b.at(i));
+        if (d>eps) {
+            cout<<std::scientific << std::setprecision(16);
+            cout<<i<<' '<<a.at(i)<<' '<<b.at(i)<<' '<<d<<endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<double> v_sqrt {1.2619825672330027e+00  ,
+0.0000000000000000e+00  ,
+2.1354098463568110e+00  ,
+3.9620198644763360e-01  ,
+0.0000000000000000e+00  ,
+-1.8066712371137301e+00 ,
+0.0000000000000000e+00  ,
+-6.7041625215270972e-01 ,
+0.0000000000000000e+00  ,
+3.0570814914830691e+00  ,
+-6.2194208597204828e-02 ,
+0.0000000000000000e+00  ,
+1.7016242971685265e+00  ,
+0.0000000000000000e+00  ,
+-6.4661371792376841e+00 ,
+0.0000000000000000e+00  ,
+3.1571781308208735e-01  ,
+0.0000000000000000e+00  ,
+-4.7988846720505292e+00 ,
+0.0000000000000000e+00  ,
+1.5317956604581280e+01 };
+
+
 
 TEST_CASE("INITIALIZE DA ENVIRONMENT") {
-    int da_dim = 3;
-    int da_order = 4;
+    int da_dim = 2;
+    int da_order = 5;
     int n_vec = 400;
     int init = SymbDA::da_init(da_order, da_dim, n_vec);
     REQUIRE( init  == 0);
 }
 
-// TEST_CASE("DA FUNCTIONS") {
-//     DAVector x = 1 + da[0] + 2*da[1] + 5*da[2];
-//     DAVector y = exp(x);
-//     double eps = 1e-14;
+TEST_CASE("SDA FUNCTIONS") {
+    auto& sda = SymbDA::da;
 
-//     SECTION("EXP") {
-//         REQUIRE(compare_da_with_file("exp_da.txt", y, eps));
-//     }
+    Expression sx("x"), sy("y");
+    double x{1.5926}, y{5.3897};
+    double eps = 1e-15;
 
-// 	DAVector z;
-//     da_substitute(y, 0, 1, z);
+    SDA sv = sqrt(sx + sda[0]*sda[0] + sy*sda[1]);
+    vector<SymEngine::RCP<const SymEngine::Basic>> vars{sx,sy};
+    SymEngine::LambdaRealDoubleVisitor vec;
+    sv.eval_funs(vars, vec);
+    vector<double> results;
+    results.resize(sv.full_length());
+    array<double, 2> inputs{0,0};
+    inputs.at(0) = x;
+    inputs.at(1) = y;
+    
+    cout<<"===== Testing math function: sqrt() ====="<<endl;
+    vec.call(&results[0], &inputs[0]);
 
-//     SECTION("SUBSTITUTE A NUMBER") {
-//         REQUIRE(compare_da_with_file("substitute_number.txt", z, eps));
-//     }
+    SECTION("SQRT") {
+        REQUIRE(compare_vectors(v_sqrt, results, eps));
+    }
 
-//     da_substitute(y, 0, x, z);
+	// DAVector z;
+    // da_substitute(y, 0, 1, z);
 
-//     SECTION("SUBSTITUTE A DA VECTOR") {
-//         REQUIRE(compare_da_with_file("substitute_da_vector.txt", z, eps));
-//     }
+    // SECTION("SUBSTITUTE A NUMBER") {
+    //     REQUIRE(compare_da_with_file("substitute_number.txt", z, eps));
+    // }
 
-// 	std::vector<DAVector> lv(2);
-//     lv.at(0) = sin(x);
-//     lv.at(1) = cos(x);
-//     std::vector<unsigned int> idx{0,1};
-//     da_substitute(y, idx, lv, z);
-//     SECTION("SUBSTITUTE MULTIPLE DA VECTORS") {
-//         REQUIRE(compare_da_with_file("substitute_multiple_da_vectors.txt", z, eps));
-//     }
+    // da_substitute(y, 0, x, z);
 
-// 	std::vector<DAVector> lx(3);
-//     std::vector<DAVector> ly(3);
-//     lx.at(0) = x;
-//     lx.at(1) = y;
-//     lx.at(2) = sinh(x);
+    // SECTION("SUBSTITUTE A DA VECTOR") {
+    //     REQUIRE(compare_da_with_file("substitute_da_vector.txt", z, eps));
+    // }
 
-//     da_substitute(lx, idx, lv, ly);
+	// std::vector<DAVector> lv(2);
+    // lv.at(0) = sin(x);
+    // lv.at(1) = cos(x);
+    // std::vector<unsigned int> idx{0,1};
+    // da_substitute(y, idx, lv, z);
+    // SECTION("SUBSTITUTE MULTIPLE DA VECTORS") {
+    //     REQUIRE(compare_da_with_file("substitute_multiple_da_vectors.txt", z, eps));
+    // }
 
-//     SECTION("BUNCH SUBSTITUTION") {
-//         REQUIRE(compare_da_with_file("bunch_substitution_0.txt", ly.at(0), eps));
-//         REQUIRE(compare_da_with_file("bunch_substitution_1.txt", ly.at(1), eps));
-//         REQUIRE(compare_da_with_file("bunch_substitution_2.txt", ly.at(2), eps));
-//     }
+	// std::vector<DAVector> lx(3);
+    // std::vector<DAVector> ly(3);
+    // lx.at(0) = x;
+    // lx.at(1) = y;
+    // lx.at(2) = sinh(x);
 
-// 	std::vector<DAVector> lu(3);
-//     lu.at(0) = sin(x);
-//     lu.at(1) = cos(x);
-//     lu.at(2) = tan(x);
-//     da_composition(lx, lu, ly);
+    // da_substitute(lx, idx, lv, ly);
 
-//     SECTION("DA COMPOSITION") {
-//         REQUIRE(compare_da_with_file("da_composition_0.txt", ly.at(0), eps));
-//         REQUIRE(compare_da_with_file("da_composition_1.txt", ly.at(1), eps));
-//         REQUIRE(compare_da_with_file("da_composition_2.txt", ly.at(2), eps));
-//     }
+    // SECTION("BUNCH SUBSTITUTION") {
+    //     REQUIRE(compare_da_with_file("bunch_substitution_0.txt", ly.at(0), eps));
+    //     REQUIRE(compare_da_with_file("bunch_substitution_1.txt", ly.at(1), eps));
+    //     REQUIRE(compare_da_with_file("bunch_substitution_2.txt", ly.at(2), eps));
+    // }
 
-// }
+	// std::vector<DAVector> lu(3);
+    // lu.at(0) = sin(x);
+    // lu.at(1) = cos(x);
+    // lu.at(2) = tan(x);
+    // da_composition(lx, lu, ly);
+
+    // SECTION("DA COMPOSITION") {
+    //     REQUIRE(compare_da_with_file("da_composition_0.txt", ly.at(0), eps));
+    //     REQUIRE(compare_da_with_file("da_composition_1.txt", ly.at(1), eps));
+    //     REQUIRE(compare_da_with_file("da_composition_2.txt", ly.at(2), eps));
+    // }
+
+}
 
 // TEST_CASE("CD FUNCTIONS") {
 //     double eps = 1e-14;
